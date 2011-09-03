f2b8e7632ee1f3460998641fdb3417d0052c81db
Steffen Vogel <info@steffenvogel.de>
2011-09-03 15:16:51 +0200

    added dokuwiki template

---
+++ dw_tpl/main.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * DokuWiki Default Template
+ *
+ * This is the template you need to change for the overall look
+ * of DokuWiki.
+ *
+ * You should leave the doctype at the very top - It should
+ * always be the very first line of a document.
+ *
+ * @link   http://dokuwiki.org/templates
+ * @author Andreas Gohr <andi@splitbrain.org>
+ */
+
+// must be run from within DokuWiki
+if (!defined('DOKU_INC')) die();
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $conf['lang']?>"
+ lang="<?php echo $conf['lang']?>" dir="<?php echo $lang['direction']?>">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>
+    <?php tpl_pagetitle()?>
+    [<?php echo strip_tags($conf['title'])?>]
+  </title>
+
+  <?php tpl_metaheaders()?>
+
+  <link rel="shortcut icon" href="<?php echo tpl_getFavicon() ?>" />
+</head>
+<body>
+
+<?php html_msgarea()?>
+
+<?php include(dirname(__FILE__).'/header.html')?>
+
+<div class="dokuwiki">
+  <div class="stylehead">
+
+    <div class="bar" id="bar__top">
+     <div class="container_12">
+      <div class="bar-left grid_8" id="bar__topleft">
+       <?php if($conf['breadcrumbs']){?>
+        <div class="breadcrumbs">
+          <?php tpl_breadcrumbs()?>
+          <?php //tpl_youarehere() //(some people prefer this)?>
+        </div>
+       <?php }?>
+
+       <?php if($conf['youarehere']){?>
+        <div class="breadcrumbs">
+          <?php tpl_youarehere() ?>
+        </div>
+       <?php }?>
+      </div>
+
+      <div class="bar-right grid_4" id="bar__topright">
+        <?php tpl_searchform()?>&nbsp;
+      </div>
+
+      <div class="clearer"></div>
+     </div>
+    </div>
+  </div>
+  <?php tpl_flush()?>
+
+  <?php /*old includehook*/ @include(dirname(__FILE__).'/pageheader.html')?>
+
+  <div class="page container_12">
+   <div class="grid_12">
+    <?php tpl_content()?>
+   </div>
+  </div>
+
+  <div class="clearer"></div>
+
+  <?php tpl_flush()?>
+
+  <div class="stylefoot">
+    <div class="bar" id="bar__bottom">
+     <div class="container_12">
+      <div class="bar-left" id="bar__bottomleft">
+       <?php tpl_button('login')?>
+        <?php tpl_button('admin')?>
+        <?php tpl_button('profile')?>
+        <?php tpl_button('subscribe')?>
+      </div>
+      <div class="bar-right" id="bar__bottomright">
+        <?php tpl_button('edit')?>
+        <?php tpl_button('history')?>
+        <?php tpl_button('recent')?>
+        <?php tpl_button('index')?>
+        <?php tpl_button('revert')?>
+        
+        &nbsp;
+      </div>
+      <div class="clearer"></div>
+     </div>
+    </div>
+  </div>
+</div>
+
+<?php include(dirname(__FILE__).'/footer.html')?>
+
+<div class="container_12">
+  <div class="meta">
+    <div class="user">
+      <?php tpl_userinfo()?>
+    </div>
+    <div class="doc">
+      <?php tpl_pageinfo()?>
+    </div>
+  </div>
+</div>
+
+<div class="no"><?php /* provide DokuWiki housekeeping, required in all templates */ tpl_indexerWebBug()?></div>
+</body>
+</html>
