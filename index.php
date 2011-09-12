b65f3dee4b6cf440492045c3d21372ba67999272
Steffen Vogel <info@steffenvogel.de>
2011-09-12 04:10:42 +0200

    splited template in multiple files

--- index.php
+++ index.php
@@ -8,71 +8,38 @@
 	<title>volkszaehler.org</title>
 	<link rel="stylesheet" href="css/normalize.css" type="text/css" media="screen" title="no title" charset="utf-8">
 	<link rel="stylesheet" href="css/960.css" type="text/css" media="screen" title="no title" charset="utf-8">
+	<link rel="stylesheet" href="css/jquery.tweet.css" type="text/css" media="screen" title="no title" charset="utf-8">
 	<link rel="stylesheet" href="css/default.css" type="text/css" media="screen" title="no title" charset="utf-8">
-	
+
 	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 	<link rel="apple-touch-icon" href="favicon.png" />
 </head>
 
 <body>
 
-<?php @include('header.html'); ?>
+<?php
 
-<div class="teaser">
-	<div class="container_12">
-		<div class="grid_4">
-			<h2>Stromverbrauch im &Uuml;berblick</h2>
-			<p>volkszaehler.org ist ein freier Smart Meter (hier: intelligenter Stromzähler) im Selbstbau. Die anfallenden Stromprofile bleiben dabei unter der Kontrolle des Nutzers.</p>
-			<a href="https://demo.volkszaehler.org/frontend/?uuid=6836dd20-00d5-11e0-bab1-856ed5f959ae" class="teaser_button">Jetzt ausprobieren!</a>
-		</div>
-		<div class="grid_8">
-			<div class="screenshot floatRight">
-				<img src="images/screenshot.png" alt="screenshot" />
-			</div>
-		</div>
-	</div>
-</div>
-<div class="container_12">
-	<div class="grid_12">
-		<h2>Allgemeine Informationen</h2>
-		<p>Seit dem 1.1.2010 müssen Stromversorger ihren Kunden für Neubauten so genannte "intelligente Stromzähler" (Smart Meter) anbieten. Der Kunde soll dadurch seinen Stromverbrauch analysieren und optimieren können.</p>
-		<p>Die dabei anfallenden Daten ("Stromverbrauchsprofil") erlauben einen sehr detallierten Einblick in den Tagesablauf des Nutzers (wann steht er auf? wann geht er in's Bett? wann kocht er? wie oft verwendet er seine Spülmaschine? verändert sich sein Verhalten? ...).</p>
-		<p>Darum sollten die Daten ausschließlich für den Nutzer selbst zur Verfügung stehen - und das geht nur, wenn man sich den Smart Meter selbst baut. Mit einem Materialeinsatz von ca. EUR 100, etwas Geschick und Zeit lässt sich das mit Hilfe eines Standard-µC-Moduls aufbauen.</p>
-	</div>
-</div>
-<div class="container_12">
-	<div class="grid_3"><div class="box">
-		<h3>Wiki</h3>
-		<p>Hier findest du alles, was es über volkszaehler.org zu wissen gibt. Anleitungen, Hintergrundwissen, etc</p>
-	</div></div>
-	<div class="grid_3"><div class="box">
-		<h3>Anleitung</h3>
-		<p>Kurz und knapp, eine Schritt-für-Schritt Anleitung für jeden mit ein wenig handwerklichem Geschick.</p>
-	</div></div>
-	<div class="grid_3"><div class="box">
-		<h3>Mitwirken</h3>
-		<p>Du bist Entwickler und willst helfen volkszaehler.org zu verbessern? Den kompletten Quelltext findest du in unserem GitHub Repository.</p>
-	</div></div>
-	<div class="grid_3"><div class="box">
-		<h3>Mailing-Liste</h3>
-		<p>Du interessierst dich für die Entwicklung von volkszaehler.org? Abonniere die Mailing-Liste und bleibe immer auf dem neusten Stand.</p>
-	</div></div>
-</div>
-<div class="container_12">
-	<div class="grid_6"><div class="box">
-		<h3>Aktuelles</h3>
-		<p><strong>Wir suchen Helfer!</strong> <br />
-		Die Arbeit an volkszaehler.org erfordert eine Menge Zeit. Wenn du denkst, du kannst etwas zu dem Projekt beitragen, sieh doch mal nach, was es noch zu tun gibt.</p>
-	</div></div>
-	<div class="grid_3"><div class="box">
-		<h3>Twitter</h3>
-	</div></div>
-	<div class="grid_3"><div class="box">
-		<h3>IRC</h3>
-	</div></div>
-</div>
+define('DIR', dirname(__FILE__));
 
-<?php @include('footer.html'); ?>
+include(DIR . '/header.html');
+
+if (empty($_SERVER['PATH_INFO'])) {
+	include(DIR . '/home.html.inc');
+}
+else {
+	$file = DIR . '/' . $_SERVER['PATH_INFO'] . '.html.inc';
+
+	if (file_exists($file)) {
+		include($file);
+	}
+	else {
+		include(DIR . '/404.html.inc');
+	}
+}
+
+include(DIR . '/footer.html');
+
+?>
 
 </body>
 </html>
